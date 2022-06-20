cbf264e059835cb947578a04e449bfb2b98b19d6
Ship-Cheaper <git@ship-cheaper.com>
2022-06-20 16:32:48 +0200

    added Threads

--- src/Chatogram/Server/Program.java
+++ src/Chatogram/Server/Program.java
@@ -1,7 +1,30 @@
 package Chatogram.Server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;;
+
 public class Program {
-    public static void main(String[] args) {
-        Server server = new Server();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSockets = new ServerSocket(4999);
+        while(true){
+            Socket socket = null;
+            try {
+                socket = serverSockets.accept();
+                System.out.println("A new client is connected: " + socket);
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                Server server = new Server(socket, objectInputStream, objectOutputStream);
+                Thread thread = new Thread(server);
+                thread.start();
+            } catch (SocketException e){
+                socket.close();
+                e.printStackTrace();
+            }
+        }
     }
 }
